07f31a32828e18892d339a1c92db023de502ebdb
fcontice <contifrank4@gmail.com>
2024-09-14 00:12:18 -0400

    fifth Timestamp getting invested i went over again

--- base/views.py
+++ base/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Room
+from .forms import RoomForm
 
 # Create your views here.
 
@@ -16,13 +17,37 @@ def home(request):
 
 def room(request, pk):
     room = Room.objects.get(id=pk)  # This line retrieves the room with the id that matches the pk parameter.
-    for i in rooms:
-        if i['id'] == int(pk):
-            room = i
     context = {'room':room}
 
     return render(request, 'base/room.html', context)
 
 def createRoom(request):
-    context = {}
+    form = RoomForm()
+    if request.method == 'POST':
+        form = RoomForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+
+    context = {'form':form}
     return render(request, 'base/room_form.html', context)
+
+def updateRoom(request, pk):
+    room = Room.objects.get(id=pk)
+    form = RoomForm(instance=room)
+    if request.method == 'POST':
+        form = RoomForm(request.POST, instance=room)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+
+    context = {'form':form}
+    return render(request, 'base/room_form.html', context)
+
+def deleteRoom(request, pk):
+    room = Room.objects.get(id=pk)
+    if request.method == 'POST':
+        room.delete()
+        return redirect('home')
+
+    return render(request, 'base/delete.html',  {'obj':room})
