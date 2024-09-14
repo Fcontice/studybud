0a3d204e7fff2e5b32122731feb92c870cf491c7
fcontice <contifrank4@gmail.com>
2024-09-13 23:49:28 -0400

    fourth timestamp a little over the timestamp

--- base/views.py
+++ base/views.py
@@ -1,22 +1,28 @@
 from django.shortcuts import render
+from .models import Room
 
 # Create your views here.
 
-rooms = [
-    {'id':1, 'name':'Lets learn Django'},
-    {'id':2, 'name':'Python is the best'},
-    {'id':3, 'name':'Javascript is the future'},
-]
+# rooms = [
+#     {'id':1, 'name':'Lets learn Django'},
+#     {'id':2, 'name':'Python is the best'},
+#     {'id':3, 'name':'Javascript is the future'},
+# ]
 
 def home(request):
+    rooms = Room.objects.all()
     context = {'rooms':rooms}
     return render(request, 'base/home.html', context)
 
 def room(request, pk):
-    room = None
+    room = Room.objects.get(id=pk)  # This line retrieves the room with the id that matches the pk parameter.
     for i in rooms:
         if i['id'] == int(pk):
             room = i
     context = {'room':room}
 
     return render(request, 'base/room.html', context)
+
+def createRoom(request):
+    context = {}
+    return render(request, 'base/room_form.html', context)
