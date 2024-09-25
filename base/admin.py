8c3eb6f5b6aaf73cb34ea2695678d3036cb7e7de
fcontice <contifrank4@gmail.com>
2024-09-24 22:42:55 -0400

    last commit part 3

--- base/admin.py
+++ base/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
 
-from .models import Room, Topic, Message
-
+from .models import Room, Topic, Message, User\
+    
+admin.site.register(User)   # This line registers the User model with the Django admin site.
 admin.site.register(Room)   # This line registers the Room model with the Django admin site.
 admin.site.register(Topic) # This line registers the Topic model with the Django admin site.
 admin.site.register(Message)     # This line registers the Message model with the Django admin site.
--- studybud/settings.py
+++ studybud/settings.py
@@ -41,7 +41,11 @@ INSTALLED_APPS = [
     'base.apps.BaseConfig',
     
     'rest_framework',
+    'corsheaders',
 ]
+
+AUTH_USER_MODEL = 'base.User'
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -123,11 +127,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = '/images/'
 
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
 
+MEDIA_ROOT = BASE_DIR / 'static/images'
 #STATIC_ROOT = 
 
 # Default primary key field type
